925dc2d68b5ed9ab77dd7979ad7b3ec6311e52c0
Khatya Khakhutaishvili <kkhatya@gmail.com>
2020-08-27 19:38:00 +0300

    bugfix/4.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let profile = document.querySelector('.profile');
-let UserName = profile.querySelector('.profile__name');
-let AboutSelf = profile.querySelector('.profile__about-self');
+let userName = profile.querySelector('.profile__name');
+let aboutSelf = profile.querySelector('.profile__about-self');
 let editButton = profile.querySelector('.profile__edit-button');
 
 let formElements = document.querySelector('.popup');
@@ -10,26 +10,25 @@ let jobInput = formElements.querySelector('.popup__item_el_about-self');
 let closeButton = formElements.querySelector('.popup__close');
 let submitButton = formElements.querySelector('.popup__button');
 
-editButton.addEventListener('click', formOpen);
-closeButton.addEventListener('click', formClose);
-formElements.addEventListener('submit', formSubmitHandler);
-
-function formOpen() {
-  formElements.classList.add('popup_opened');
-  nameInput.value = UserName.textContent;
-  jobInput.value = AboutSelf.textContent;
-}
-
-function formClose() {
-  formElements.classList.remove('popup_opened');
+function formStatus() {
+  formElements.classList.toggle('popup_opened');
+  if (formElements.classList.contains('popup_opened')) {
+    nameInput.value = userName.textContent;
+    jobInput.value = aboutSelf.textContent;
+  }
 }
 
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    UserName.textContent = nameInput.value;
-    AboutSelf.textContent = jobInput.value;
-    formElements.classList.remove('popup_opened');
+    userName.textContent = nameInput.value;
+    aboutSelf.textContent = jobInput.value;
+    formStatus()
 }
+
+
+editButton.addEventListener('click', formStatus);
+closeButton.addEventListener('click', formStatus);
+formElements.addEventListener('submit', formSubmitHandler);
 
